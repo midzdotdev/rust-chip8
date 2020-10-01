948bc4cd33a213d3c3b2badf5de69285e89a2058
James Middleton <hello@jamesmiddleton.me>
2020-10-02 00:01:32 +0100

    ✨ Implement some instructions

--- src/cpu.rs
+++ src/cpu.rs
@@ -21,6 +21,54 @@ impl Cpu {
   }
 
   pub fn run_instruction(&mut self, ram: &mut Ram) {
+    let address = self.pc;
 
-  } 
+    let hi = ram.read_byte(address) as u16;
+    let lo = ram.read_byte(address+1) as u16;
+
+    let instruction: u16 = (hi << 8) | lo;
+
+    println!("Address: {:#X}, Instruction: {:#X}", address, instruction);
+
+    let nnn = instruction & 0x0FFF;
+    let kk = (instruction & 0x00FF) as u8;
+    let n = (instruction & 0x000F) as u8;
+    let x = instruction & 0x0F00 >> 8;
+    let y = instruction & 0x00F0 >> 4;
+
+    println!("nnn: {:#X}, kk: {:?}, n: {:?}, x: {}, y: {}", nnn, kk, n, x, y);
+
+    let mut next_pc = self.pc + 2;
+
+    match (instruction & 0xF000) >> 12 {
+      0x1 => {
+        // 1nnn: goto nnn
+        next_pc = nnn;
+      },
+      0x6 => {
+        // 6xkk: vx = kk
+        self.write_reg_vx(x, kk);
+      },
+      0xA => {
+        // Annn: I = nnn
+        self.i = nnn;
+      },
+
+      _ => panic!("Unknown instruction {:#X}", instruction)
+    }
+
+    if self.pc == next_pc {
+      panic!("Infinite loop: PC has not changed");
+    }
+
+    self.pc = next_pc;
+  }
+
+  fn write_reg_vx(&mut self, index: u16, value: u8) {
+    self.vx[index as usize] = value;
+  }
+
+  fn read_reg_vx(self, index: u16) -> u8 {
+    return self.vx[index as usize]
+  }
 }
